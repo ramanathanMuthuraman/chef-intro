c2df81545f33b0267318ff5e145954c0690bd362
Chirantan Mitra <Chirantan_Mitra@external.mckinsey.com>
2018-10-25 11:52:39 +0530

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# chef_example
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: chef_example
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
